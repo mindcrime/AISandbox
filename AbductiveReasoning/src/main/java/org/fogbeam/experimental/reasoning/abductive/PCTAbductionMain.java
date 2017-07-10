7ae5f6469f8c9d7515a2e264686e48d58be4067a
Phillip Rhodes <motley.crue.fan@gmail.com>
2017-07-09 20:41:59 -0400

    start adding classes to represent the various operations that make up the PCT algorithm - div, res, augres, revise, and so forth

---
+++ AbductiveReasoning/src/main/java/org/fogbeam/experimental/reasoning/abductive/PCTAbductionMain.java
@@ -0,0 +1,27 @@
+package org.fogbeam.experimental.reasoning.abductive;
+
+import org.eclipse.collections.impl.set.mutable.UnifiedSet;
+
+public class PCTAbductionMain 
+{
+
+	public static void main(String[] args) 
+	{
+
+		// start our initial mPlus set
+		UnifiedSet<String> mPlus = new UnifiedSet<String>();
+		
+		// prompt user for Manifestations
+		// until user says 'q'
+		
+		// run through the PCT / BIPARTITE algorithm
+		// generating all the plausible explanations
+		
+		
+		
+		
+		
+		System.out.println( "done" );
+	}
+
+}
---
+++ AbductiveReasoning/src/main/java/org/fogbeam/experimental/reasoning/abductive/queries/QueryCauses.java
@@ -0,0 +1,91 @@
+package org.fogbeam.experimental.reasoning.abductive.queries;
+
+import static org.fogbeam.experimental.reasoning.abductive.AbductionConstants1.TDB_DIR;
+
+import java.io.File;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.fogbeam.experimental.reasoning.abductive.AbductionConstants1.RESOURCE_BASE;
+import org.apache.jena.query.Dataset;
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.rdf.model.InfModel;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.reasoner.Reasoner;
+import org.apache.jena.reasoner.ReasonerRegistry;
+import org.apache.jena.tdb.TDBFactory;
+import org.eclipse.collections.impl.set.mutable.UnifiedSet;
+
+public class QueryCauses 
+{
+	public UnifiedSet<String> doQuery( final Set<String> manifestations )
+	{
+		UnifiedSet<String> causes = new UnifiedSet<String>();
+		
+		
+		File tdbDir = new File(TDB_DIR);
+		if( !tdbDir.exists())
+		{
+			tdbDir.mkdirs();
+		}
+		
+		Dataset ds = TDBFactory.createDataset(TDB_DIR);
+		Model model = ds.getDefaultModel();
+		
+		
+		Reasoner reasoner = ReasonerRegistry.getOWLReasoner();
+		InfModel infmodel = ModelFactory.createInfModel(reasoner, model );		
+		
+
+		for( String manifestation : manifestations )
+		{
+			
+			UnifiedSet<String> disorders = new UnifiedSet<String>();
+			
+			// build up our SPARQL query for all entries in mPlus
+			
+			String sparqlQuery = "select ?disorder where {" + ( " ?disorder <" + RESOURCE_BASE + "#causes>" + " <" + RESOURCE_BASE + "/manifestation#" + manifestation + "> . }" );
+					
+		
+			// System.out.println( "query: \n" + sparqlQuery + "\n" );
+		
+			// execute the query
+			Query query = QueryFactory.create(sparqlQuery);
+			QueryExecution qexec = QueryExecutionFactory.create(query, infmodel) ;
+	
+			try
+			{
+			    ResultSet resultSet = qexec.execSelect() ;
+			    for ( ; resultSet.hasNext() ; )
+			    {
+			    	QuerySolution soln = resultSet.nextSolution() ;
+			    	Resource m = soln.getResource("disorder");
+			      
+			    	String res = m.toString();
+			    	// System.out.println( res );
+		
+					// plug query results in our results
+			    	causes.add( res );
+			    
+			    }
+			}
+			finally
+			{
+				qexec.close();
+			}			
+		}
+		
+		infmodel.close();	
+
+		ds.close();
+		
+		return causes;
+	}
+}
