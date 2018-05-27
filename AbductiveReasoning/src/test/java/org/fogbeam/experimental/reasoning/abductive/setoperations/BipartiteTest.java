edc50c7ee7baa756713b9e6da28eb82411a6306b
Phillip Rhodes <motley.crue.fan@gmail.com>
2018-05-26 20:40:21 -0400

    start adding unit tests

---
+++ AbductiveReasoning/src/test/java/org/fogbeam/experimental/reasoning/abductive/setoperations/BipartiteTest.java
@@ -0,0 +1,15 @@
+package org.fogbeam.experimental.reasoning.abductive.setoperations;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class BipartiteTest
+{
+	@Test
+	public void testFoo()
+	{
+		assertTrue( true );
+	}
+}
